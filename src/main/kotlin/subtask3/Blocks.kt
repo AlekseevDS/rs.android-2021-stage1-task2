2ce3b8829b21d8ea77fa4f3615cc47090f0b77e6
AlekseevDS <alekseevdms@gmail.com>
2021-06-12 20:05:38 +0300

    feat: add solution Abbreviation and Blocks

--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -1,11 +1,46 @@
 package subtask3
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
 
 class Blocks {
 
-    // TODO: Complete the following function
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+        if (blockB.equals(String::class)) {
+            val strList = blockA.filterIsInstance<String>()
+            var resultStr = ""
+            for (i in strList) {
+                resultStr = resultStr.plus(i)
+            }
+            return resultStr
+        } else if (blockB.equals(Int::class)) {
+            val intList = blockA.filterIsInstance<Int>()
+            var resultInt = 0
+            for (i in intList) {
+                resultInt += i
+            }
+            return resultInt
+        } else if (blockB.equals(LocalDate::class)) {
+            val dateList = blockA.filterIsInstance<LocalDate>()
+            var resultDate: LocalDate = LocalDate.now()
+            var tmpDate = 0L
+            for (i in dateList) {
+                if (i.toEpochDay() > tmpDate) {
+                    tmpDate = i.toEpochDay()
+                    resultDate = LocalDate.parse(i.toString())
+                }
+            }
+
+            return resultDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+        }
+        return "no data"
     }
 }
+
+
+
+
+
+
+
