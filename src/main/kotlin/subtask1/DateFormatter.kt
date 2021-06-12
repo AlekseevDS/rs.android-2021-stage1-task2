96be0ef0b23b1610eb8ce2b409f92bdb2a03279c
AlekseevDS <alekseevdms@gmail.com>
2021-06-12 15:44:32 +0300

    feat: add solution DateFormatter

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,29 @@
 package subtask1
 
+import java.time.DateTimeException
+import java.time.LocalDate
+import java.time.format.TextStyle
+import java.util.*
+
+
 class DateFormatter {
 
-    // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+
+        val rusLocale: Locale  = Locale.Builder().setLanguage("ru").setScript("Cyrl").build();
+
+        try {
+            val date = LocalDate.of(year.toInt(), month.toInt(), day.toInt())
+            val text_day_of_the_week = date.getDayOfWeek().getDisplayName(TextStyle.FULL, rusLocale).toLowerCase()
+            val text_month = date.month.getDisplayName(TextStyle.FULL, rusLocale).toLowerCase()
+            val result = "$day $text_month, $text_day_of_the_week"
+            return result
+
+        } catch (e: DateTimeException) {
+            return "Такого дня не существует"
+            //e.printStackTrace();
+
+        }
+
     }
 }
